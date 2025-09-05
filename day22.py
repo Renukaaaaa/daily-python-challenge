c4dc32961fc55c908e0df96b0a81ce8df256da26
renu_patil <134476104+Renukaaaaa@users.noreply.github.com>
2025-09-05 10:30:34 +0530

    Add files via upload

---
+++ day22.py
@@ -0,0 +1,11 @@
+def first_element_k_times(arr, k):
+    count = {}
+    for num in arr:
+        count[num] = count.get(num, 0) + 1
+    for num in arr:
+        if count[num] == k:
+            return num
+    return -1
+arr = [3, 1, 4, 4, 5, 2, 6, 1, 4]
+k = 2
+print(first_element_k_times(arr, k))
