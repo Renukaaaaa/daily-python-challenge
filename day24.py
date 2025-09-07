11cd531e8eb668254daafd9970b7edde8e317b2b
renu_patil <134476104+Renukaaaaa@users.noreply.github.com>
2025-09-07 12:32:51 +0530

    Add files via upload

---
+++ day24.py
@@ -0,0 +1,30 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        return root if left and right else left or right
+
+root = TreeNode(3)
+root.left = TreeNode(5)
+root.right = TreeNode(1)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.right.left = TreeNode(0)
+root.right.right = TreeNode(8)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(4)
+
+p = root.left       
+q = root.right       
+solution = Solution()
+lca = solution.lowestCommonAncestor(root, p, q)
+print("Lowest Common Ancestor:", lca.val)  
